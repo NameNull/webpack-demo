5ec3e00ecbfeba157e556302f0426c3034b0bce3
yujie <945351749@qq.com>
2017-08-23 20:17:51 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './cat.jpg';
+
+function component() {
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['Hello','webpack'],' ');
+    element.classList.add('hello')
+    var icon = new Image();
+    icon.src = Icon;
+    element.appendChild(icon);
+    return element;
+}
+document.body.appendChild(component());
